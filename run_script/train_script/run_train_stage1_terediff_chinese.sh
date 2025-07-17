c532d2574e134af088d1602011497dae9bdc2b9d
qcanic <qcanic@gmail.com>
2025-07-18 01:03:34 +0800

    update

--- run_script/train_script/run_train_stage1_terediff_chinese.sh
+++ run_script/train_script/run_train_stage1_terediff_chinese.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Stage 1: 训练图像恢复模块（支持中文）
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 29600 train.py \
+CUDA_VISIBLE_DEVICES=0 accelerate launch train.py \
     --config configs/train/train_stage1_terediff_chinese.yaml \
     --config_testr testr/configs/TESTR/TESTR_R_50_Polygon_Chinese.yaml
